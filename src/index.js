60e27180807271e943ddb84d1affdf25291d2689
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-05-11 16:51:06 +0530

    secnd change

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import app from "./app.js"
 import dotenv from "dotenv"
 import connectDB from './db/dbconnection.js'
 
+let PORT=process.env.PORT || 4000
 dotenv.config({
     path:'./.env'
 })
 
 connectDB().then(()=>{
-  app.listen(process.env.PORT,()=>{
-    console.log(`server is running at port${process.env.PORT}`);
+  app.listen(PORT,()=>{
+    console.log(`server is running at port${PORT}`);
   })
 })
 .catch((err)=>{
