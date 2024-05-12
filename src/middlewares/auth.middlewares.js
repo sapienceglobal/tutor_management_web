37c7866bead481b628946a066211acaa69ee13ce
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-05-12 09:18:40 +0530

    changeess

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,32 +1,63 @@
-import jwt from 'jsonwebtoken';
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from '../utils/apiError.js';
-import { User } from '../models/users.model.js';
+import jwt from 'jsonwebtoken'
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from '../utils/apiError.js'
+import { User } from '../models/users.model.js'
 
-export const verifyjwt = asyncHandler(async (req, res, next) => {
-    try {
-        const authHeader = req.header("Authorization");
 
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            console.log("Unauthorized request");
-            return res.json({statu:400, message:"Unauthorized request"});
-        }
-
-        const accessToken = authHeader.replace('Bearer ', ''); // Extract the token
-
-        let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        if (!decoded) {
-            return res.json({statu:400, message:"Invalid access token"});
-        }
-
-        const user = await User.findById(decoded._id).select("-password -refreshToken");
-        if (!user) {
-            return res.json({statu:400, message:"Invalid access token"});
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.json({status:401, message:error?.message || "Invalid access token"});
+export const verifyjwt=asyncHandler(async(req,res,next)=>{
+   try {
+     const accessToken=req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+     if (!accessToken) {
+        return res.json({statu:400, message:"Unauthorized request"});
     }
-});
+     let decode=jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET)
+     if(!decode){
+        return res.json({statu:400, message:"Invalid access token"});
+     }
+    const user= await User.findById(decode._id).select("-password -refreshToken")
+    if (!user) { 
+        return res.json({statu:400, message:"Invalid access token"});
+    }
+    req.user=user
+    next()
+   } catch (error) {
+    return res.json({status:401, message:error?.message || "Invalid access token"});
+    
+   }
+
+})
+
+
+
+// import jwt from 'jsonwebtoken';
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import { ApiError } from '../utils/apiError.js';
+// import { User } from '../models/users.model.js';
+
+// export const verifyjwt = asyncHandler(async (req, res, next) => {
+//     try {
+//         const authHeader = req.header("Authorization");
+
+//         if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//             console.log("Unauthorized request");
+//             return res.json({statu:400, message:"Unauthorized request"});
+//         }
+
+//         const accessToken = authHeader.replace('Bearer ', ''); // Extract the token
+
+//         let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+//         if (!decoded) {
+//             return res.json({statu:400, message:"Invalid access token"});
+//         }
+
+//         const user = await User.findById(decoded._id).select("-password -refreshToken");
+//         if (!user) {
+//             return res.json({statu:400, message:"Invalid access token"});
+//         }
+
+//         req.user = user;
+//         next();
+//     } catch (error) {
+//         return res.json({status:401, message:error?.message || "Invalid access token"});
+//     }
+// });
