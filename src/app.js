4dad200deda6da8306c4095bb9dd4cde6e3364fe
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-05-12 11:10:04 +0530

    aa

--- src/app.js
+++ src/app.js
@@ -4,24 +4,10 @@ import cookieParser from 'cookie-parser'
 
 const app=express()
 
-// app.use(cors({
-//     origin:process.env.CORS_ORIGIN,
-//     credentials:true
-// }))
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://yourfrontenddomain.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
 
 app.use(express.json({
     limit:'16kb'
