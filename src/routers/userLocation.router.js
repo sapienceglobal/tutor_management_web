dce57c04b3d3007d53a6900456224ebd6c710daa
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-05-16 13:55:34 +0530

    ff

--- src/routers/userLocation.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { verifyjwt } from "../middlewares/auth.middlewares"
-
-
-
-const userLocation=Router()
-userLocation.post("/getlocation",userRegister)
-export default userLocation
