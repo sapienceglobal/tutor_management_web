adc58767aed54fe842a89291c4582196e79ad38b
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-07-10 21:04:19 +0530

    sds

--- src/app.js
+++ src/app.js
@@ -1,44 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors"
+import cookieParser from 'cookie-parser'
 
-const app = express();
-
+const app=express()
 const allowedOrigins = ['https://sprcbaghpat.vercel.app'];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
+app.use(cors({
+    origin:allowedOrigins,
+    credentials:true,
     optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-
-// Add this to handle preflight requests
-app.options('*', cors(corsOptions));
+}))
 
 app.use(express.json({
-    limit: '16kb'
-}));
+    limit:'16kb'
+}))
+app.use(express.urlencoded(
+    {
+        extended:true,
+        limit:'16kb'
+    }))
 
-app.use(express.urlencoded({
-    extended: true,
-    limit: '16kb'
-}));
+app.use(express.static("public"))
+app.use(cookieParser())
 
-app.use(express.static('public'));
-app.use(cookieParser());
+import userRouter from "./routers/user.router.js";
+import verifyEmailRouter from "./routers/verifyEmail.router.js";
+app.use("/api/v1/users",userRouter)
+app.use("/api/v1/verifyEmail",verifyEmailRouter)
 
-import userRouter from './routers/user.router.js';
-import verifyEmailRouter from './routers/verifyEmail.router.js';
-
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/verifyEmail', verifyEmailRouter);
-
-export default app;
+export default app
