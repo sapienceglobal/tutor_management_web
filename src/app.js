eb635909dc8481aff3d4a4e89e9f958af0faef3b
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-07-19 21:20:17 +0530

    sd

--- src/app.js
+++ src/app.js
@@ -14,8 +14,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Middleware for handling preflight requests
-app.options('*', cors(corsOptions)); 
+// Middleware for handling preflight requests for specific routes
+app.options('/api/v1/users/*', cors(corsOptions)); // Preflight for user routes
+app.options('/api/v1/verifyEmail/*', cors(corsOptions)); // Preflight for verifyEmail routes
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
