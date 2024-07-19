78ee131cf50f196308d8d1ce8f96fefe2ef20770
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-07-19 21:46:40 +0530

    sa

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import {User} from "../models/users.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import verifyonsignup from "../models/verifyEmail.model.js"
 import jwt from 'jsonwebtoken'
-
+import admissionForm from "../models/admissionForm.model.js"
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -271,7 +271,21 @@ const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({ text: "logout" });
 });
 
+const admissionForm=asyncHandler(async(req,res)=>{
+const {fullName,FathersName,MobileNo,Location}=req.body;
+if ([fullName, FathersName, MobileNo, Location].some((field) => field?.trim() === "")) {
+  return res.send({ errorMessage: "All fields are required" });
+}
+await admissionForm.create({
+  fullName,
+  fathersName:FathersName,
+  mobile:MobileNo,
+  location:Location
+})
+res.send({ message: "Resgistraion Successfull"});
+})
 
 
 
-export {userRegister,loginUser,checkUser,logoutUser,editUserDetails,updateLocation}
+
+export {userRegister,loginUser,checkUser,logoutUser,editUserDetails,updateLocation,admissionForm}
