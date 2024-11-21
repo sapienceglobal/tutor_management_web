e87e01cccbd1e9388f0f19a69b82fc756fb1f5ad
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-11-21 13:22:45 +0530

    gfg

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,19 +1,24 @@
-import {upload} from "../middlewares/multer.middlewares.js"
-import {userRegister,loginUser,checkUser,logoutUser,editUserDetails,updateLocation,admissionForm} from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middlewares.js"
+import { userRegister, loginUser, checkUser, logoutUser, editUserDetails, updateLocation, admissionForm } from "../controllers/user.controller.js"
 import { Router } from "express"
 import { verifyjwt } from "../middlewares/auth.middlewares.js"
 import path from "path";
- 
-const userRouter=Router()
-userRouter.post("/registerUser",upload.single("image"),userRegister)
-userRouter.post("/loginUser",loginUser)
+import { fileURLToPath } from 'url';
+
+
+
+const userRouter = Router()
+userRouter.post("/registerUser", upload.single("image"), userRegister)
+userRouter.post("/loginUser", loginUser)
 // userRouter.post("/loginUserByGoogle",loginUserByGoogle)
-userRouter.get("/checkuser",verifyjwt,checkUser)
-userRouter.get("/logout",verifyjwt,logoutUser)
-userRouter.post("/updatelocation",verifyjwt,updateLocation)
-userRouter.post("/admissionform",admissionForm)
-userRouter.post("/editdetails",verifyjwt,upload.single("image"),editUserDetails)
+userRouter.get("/checkuser", verifyjwt, checkUser)
+userRouter.get("/logout", verifyjwt, logoutUser)
+userRouter.post("/updatelocation", verifyjwt, updateLocation)
+userRouter.post("/admissionform", admissionForm)
+userRouter.post("/editdetails", verifyjwt, upload.single("image"), editUserDetails)
 userRouter.get("/pdf/:fileName", verifyjwt, (req, res) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     const { fileName } = req.params;
     const filePath = path.join(__dirname, "../public/pdfs", fileName);
 
