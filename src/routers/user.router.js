1479b15d05841139a187125144ec71d7758210f4
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-11-21 13:29:57 +0530

    dfd

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -20,7 +20,7 @@ userRouter.get("/pdf/:fileName", verifyjwt, (req, res) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const { fileName } = req.params;
-    const filePath = path.join(__dirname, "../public/pdfs", fileName);
+    const filePath = path.join(__dirname, "../../public/pdfs", fileName);
 
     res.sendFile(filePath, (err) => {
         if (err) {
